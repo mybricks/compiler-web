b05fb9f5aa405ad7a0bc70e5516ba5dc2b31cef0
chemingjun <chemingjun975@hellobike.com>
2021-03-22 15:52:35 +0800

    fixed rxui bugs

--- build/webpack.xg.js
+++ build/webpack.xg.js
@@ -6,14 +6,14 @@ const commonCfg = require('./webpack.common')
 //process.env.NODE_ENV = 'dev'
 
 module.exports = merge(commonCfg, {
-  entry: './demo/main.tsx',
+  entry: './_demo/main.tsx',
   output: {
-    path: path.resolve(__dirname, '../demo'),
+    path: path.resolve(__dirname, '../_demo'),
     filename: './bundle.js',
     libraryTarget: 'umd'
   },
   externals: [{
-    '@mybricks/rxui':'@mybricks/rxui',
+    // '@mybricks/rxui':'@mybricks/rxui',
     'React': 'React',
     'react': 'React',
     'react-dom': 'ReactDOM',
@@ -30,6 +30,7 @@ module.exports = merge(commonCfg, {
     alias: Object.assign({}, {//for dev
       'react': require('path').resolve(__dirname, '../node_modules/react'),
       'react-dom': require('path').resolve(__dirname, '../node_modules/react-dom'),
+      '@mybricks/rxui':require('path').resolve(__dirname, '../../rxui/src/'),
       '@mybricks/compiler-web':require('path').resolve(__dirname, '../index.ts'),
       '@mybricks/compiler-js': require('path').resolve(__dirname, '../../compiler-js/index.ts'),
     }),
@@ -45,7 +46,7 @@ module.exports = merge(commonCfg, {
   },
   devServer: {
     port: 8001,  //端口设置
-    contentBase: path.join(__dirname, '../demo'),
+    contentBase: path.join(__dirname, '../_demo'),
     disableHostCheck: true,
     //progress: true,
     inline: true
