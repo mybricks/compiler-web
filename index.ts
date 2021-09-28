9ae44f49808e56699ffdbbcdb404d0f450b538b5
CMJ <cmj@CMJs-MacBook-Pro.local>
2021-09-28 18:27:39 +0800

    sth

--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
-﻿export {parse} from './src/parse'
+﻿import pkg from './package.json'
+
+console.log(`${pkg.name}@${pkg.version}`)
+
+export {parse} from './src/parse'
 export {RenderReact} from './src/RenderReact'
